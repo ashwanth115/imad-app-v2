518f703f2e61dc7e606c41c4bdf804f77ee3ccca
ashwanth115 <ashwanth.sridharan1996@gmail.com>
2017-02-07 14:29:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ function Myfunction (data)
     <head>
         <title>${title}</title>
      <meta name="viewport" content="width=device-width,initial-scale=1"/>
-  
+     <link href="/ui/style.css" rel="stylesheet" />
     </head>
    
     <body>
