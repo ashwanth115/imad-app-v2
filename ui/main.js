79e74db637adb3bc5b56453149204d2350324a24
ashwanth115 <ashwanth.sridharan1996@gmail.com>
2017-02-09 20:06:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,7 @@
 console.log('Loaded!');
  
             var a=document.getElementById('ash');
+          var marginLeft=0;
            function moveright()
            {
              leftinterval=leftinterval+10;  
