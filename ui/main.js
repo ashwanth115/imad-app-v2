f6f714c1bd376160e487afbaa595ee829ff3c19b
ashwanth115 <ashwanth.sridharan1996@gmail.com>
2017-02-09 19:54:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,8 @@
 console.log('Loaded!');
+ alert("hello");
+            var a=document.getElementById("ash");
+            a.onclick=function ()
+            {
+              a.style.leftmargin="100px";  
+            };
+        
