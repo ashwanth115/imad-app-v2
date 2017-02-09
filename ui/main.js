572aaecff8417ebef8b60fe14704f794948517f1
ashwanth115 <ashwanth.sridharan1996@gmail.com>
2017-02-09 20:02:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,12 @@
 console.log('Loaded!');
  
             var a=document.getElementById('ash');
+           function moveright()
+           {
+             leftinterval=leftinterval+10;  
+             img.style.marginLeft=marginleft+'px';
+           }
+           var interval=setInterval(moveright,50);
             a.onclick=function ()
             {
               a.style.marginLeft='100px';  
